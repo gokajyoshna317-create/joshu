330ab5bd332e9b9d88dc3cae7f8189eb5bf934a6
gokajyoshna317-create <gokajyoshna317@gmail.com>
2026-08-12 11:03:07 +0530

    Add files via upload

---
+++ joshna.py
@@ -0,0 +1,20 @@
+from xml.dom.minidom import ProcessingInstruction
+
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+t=64
+float=3.14
+str="hello"
+boolean=True
+num=3+2j
+
+print(F"{t}\t{float}\t{str}\t{boolean}")
+
